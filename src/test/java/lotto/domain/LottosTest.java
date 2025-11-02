5ff6b526122470d2a020bab366c2adaebe06b73e
윤민수 <koelysa5696@naver.com>
2025-11-03 00:39:26 +0900

    test: 로또 목록 관리 테스트 전면 수정
    - 테스트 유니크랜덤테스트 로직으로 전면 수정
    -출력용 dto 클래스로 확인하는 것으로 수정

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -1,59 +1,68 @@
 package lotto.domain;
 
-import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomUniqueNumbersInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import lotto.domain.dto.LottoDTO;
 import org.junit.jupiter.api.Test;
 
 public class LottosTest {
-    private static final int FOUR = 3;
+    private static List<Integer> numbers;
 
     @Test
     void 로또_랜덤_번호가_정상적으로_생성된다() {
-        assertRandomNumberInRangeTest(() -> {
-                Lottos lottos = new Lottos(new LottoPrice(3000));
 
-                Lotto expectedLotto1 = new Lotto(List.of(1, 2, 3, 4, 5, 6));
-                Lotto expectedLotto2 = new Lotto(List.of(7, 8, 9, 10, 11, 12));
-                Lotto expectedLotto3 = new Lotto(List.of(13, 14, 15, 16, 17, 18));
+        assertRandomUniqueNumbersInRangeTest(() -> {
+                    Lottos lottos = new Lottos(new LottoPrice(3000));
 
-                List<Lotto> actualLottos = lottos.copyLottos();
+                    LottoDTO expectedDto1 = new LottoDTO(List.of(1, 2, 3, 4, 5, 6));
+                    LottoDTO expectedDto2 = new LottoDTO(List.of(7, 8, 9, 10, 11, 12));
+                    LottoDTO expectedDto3 = new LottoDTO(List.of(13, 14, 15, 16, 17, 18));
 
-                assertThat(actualLottos).containsExactly(
-                        expectedLotto1,
-                        expectedLotto2,
-                        expectedLotto3
-                );
-
-            }, 1, 2, 3, 4, 5, 6, // 1번 로또
-            7, 8, 9, 10, 11, 12, // 2번 로또
-            13, 14, 15, 16, 17, 18 // 3번 로또
+                    List<LottoDTO> lottoDTOS = lottos.getLottosDTO();
+                    assertThat(lottoDTOS).containsExactly(
+                            expectedDto1,
+                            expectedDto2,
+                            expectedDto3
+                    );
+                },
+                List.of(1, 2, 3, 4, 5, 6),
+                List.of(7, 8, 9, 10, 11, 12),
+                List.of(13, 14, 15, 16, 17, 18)
         );
     }
 
     @Test
     void 로또_랜덤_번호가_초과되면_예외가_발생한다() {
-        assertRandomNumberInRangeTest(() -> {
-                    assertThatThrownBy(() -> new Lottos(new LottoPrice(1000))
-                            .isInstanceOf(IllegalArgumentException.class);
-                }, 1, 2, 3, 4, 5, 46);
+        assertRandomUniqueNumbersInRangeTest(() -> {
+            assertThatThrownBy(() -> new Lottos(new LottoPrice(1000)))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }, List.of(1, 2, 3, 4, 20, 46));
     }
 
     @Test
     void 로또_번호_비교가_정상적으로_작동한다() {
-        assertRandomNumberInRangeTest(() -> {
-            List<Integer> lotto = Arrays.stream("1,2,3,4,5,6".split(","))
-                    .map(Integer::parseInt)
-                    .toList();
+        assertRandomUniqueNumbersInRangeTest(() -> {
+            numbers = List.of(1, 2, 3, 4, 20, 30);
             Lottos lottos = new Lottos(new LottoPrice(1000));
-            WinLotto winLotto = new WinLotto(new Lotto(lotto), new BonusNumber(10));
-            LottoResultTest lottoResult = lottos.compare(winLotto.copyWinLotto());
-            List<Integer> result = lottoResult.copyResult();
-            assertThat(result.get(FOUR)).isEqualTo(1); // 4등이 1개
-        }, 1, 2, 3, 4, 20, 30);
+            WinLotto winLotto = new WinLotto(new Lotto(numbers), new BonusNumber(10));
+            Map<Rank, Integer> lottoResult = lottos.lottosCompare(winLotto);
+            assertThat(lottoResult.get(Rank.FOUR)).isEqualTo(1); // 4등이 1개
+        }, List.of(1, 2, 3, 4, 5, 6));
+    }
+
+    @Test
+    void 보너스_당첨일_때_로또_번호_비교가_정상적으로_작동한다() {
+        assertRandomUniqueNumbersInRangeTest(() -> {
+            numbers = List.of(1, 2, 3, 4, 5, 30);
+            Lottos lottos = new Lottos(new LottoPrice(1000));
+            WinLotto winLotto = new WinLotto(new Lotto(numbers), new BonusNumber(10));
+            Map<Rank, Integer> lottoResult = lottos.lottosCompare(winLotto);
+            assertThat(lottoResult.get(Rank.FIVE_BONUS)).isEqualTo(1); // 2등이 1개
+        }, List.of(1, 2, 3, 4, 5, 10));
     }
 
 }
