f4417b39b9576ec2abccaf5d008996d1900458c2
윤민수 <koelysa5696@naver.com>
2025-11-02 22:12:32 +0900

    feat: 생성된 로또 출력용 dto 레코드

---
+++ src/main/java/lotto/domain/dto/LottoDTO.java
@@ -0,0 +1,16 @@
+package lotto.domain.dto;
+
+import java.util.List;
+
+public record LottoDTO(List<Integer> numbers) {
+
+    public LottoDTO(List<Integer> numbers) {
+        this.numbers = List.copyOf(numbers);
+    }
+
+    @Override
+    public String toString() {
+        return numbers.toString();
+    }
+
+}
