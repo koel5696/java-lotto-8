10319e4302b4b09fa111cf77e0c946fabdd0bcc3
윤민수 <koelysa5696@naver.com>
2025-11-02 22:13:53 +0900

    feat: 로또 시스템 서비스 구현
    - 로또 생성 서비스 및 반환 구현
    - 로또 추첨 서비스 구현
    - 최종 이익률 계산 서비스 구현

---
+++ src/main/java/lotto/service/LottoService.java
@@ -0,0 +1,48 @@
+package lotto.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lotto.domain.LottoPrice;
+import lotto.domain.Lottos;
+import lotto.domain.Rank;
+import lotto.domain.WinLotto;
+import lotto.domain.dto.LottoDTO;
+import lotto.domain.dto.LottoResultDTO;
+
+public class LottoService {
+
+    public Lottos generateLottoService(LottoPrice lottoPrice) {
+        return new Lottos(lottoPrice);
+    }
+
+    public LottoResultDTO DrawALottoService(Lottos lottos, WinLotto winLotto) {
+        Map<Rank, Integer> lottoResult = new HashMap<>();
+        for (Rank rank : Rank.values()) {
+            lottoResult.put(rank, 0);
+        }
+        Map<Rank, Integer> winningResults = lottos.lottosCompare(winLotto);
+        lottoResult.putAll(winningResults);
+
+        return lottos.toLottoResultDTO(lottoResult);
+    }
+
+    public List<LottoDTO> generateResult(Lottos lottos) {
+        return lottos.getLottosDTO();
+    }
+
+    public double profitCalculatorService(LottoResultDTO lottoResultDTO, LottoPrice lottoPrice) {
+        int price = lottoPrice.lottoCount() * 1000;
+        double total = 0.0;
+        Map<Rank, Integer> lottoResult = lottoResultDTO.lottoResult();
+        for (Rank rank : lottoResult.keySet()) {
+            int prize = rank.getPrize();
+            int count = lottoResult.get(rank);
+            total += prize * count;
+        }
+        double profit = (total / price) * 100;
+        return Math.round(profit * 10) / 10.0;
+    }
+
+
+}
