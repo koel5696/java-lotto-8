9c37c8f1119f9e8e14f60c50b61bb911c22a7cd0
윤민수 <koelysa5696@naver.com>
2025-11-02 22:10:19 +0900

    feat: 로또 우승 번호 관리 구현
    - 우승 번호 생성
    - 기본 번호와 보너스 번호 중복 여부 체크
    - 우승 번호와 로또 비교 요청 기능
        - 등수를 체크하고 결과 반환

---
+++ src/main/java/lotto/domain/WinLotto.java
@@ -0,0 +1,35 @@
+package lotto.domain;
+
+
+import java.util.Optional;
+
+public class WinLotto {
+    private final Lotto winLotto;
+    private final BonusNumber bonusNumber;
+
+    public WinLotto(Lotto lotto, BonusNumber bonusNumber) {
+        duplicateValidate(lotto, bonusNumber);
+        winLotto = lotto;
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void duplicateValidate(Lotto lotto, BonusNumber bonusNumber) {
+        if (lotto.contains(bonusNumber)) {
+            throw new IllegalArgumentException("[error] 보너스 번호는 기본 당첨 번호와 중복될 수 없습니다.");
+        }
+    }
+
+    public Optional<Rank> judgeWin(Lotto lotto) {
+        int matchCount = lotto.countMatches(winLotto);
+        boolean checkBonus = false;
+        if (matchCount == 5) {
+            checkBonus = secondJudgeWin(lotto);
+        }
+        return Rank.determineWinAmount(matchCount, checkBonus);
+
+    }
+
+    private boolean secondJudgeWin(Lotto lotto) {
+        return lotto.contains(bonusNumber);
+    }
+}
