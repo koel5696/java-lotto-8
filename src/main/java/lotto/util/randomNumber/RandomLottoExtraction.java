843cc6d7696ba58631a4ed9e419419821e23accb
윤민수 <koelysa5696@naver.com>
2025-11-02 22:01:45 +0900

    feat: 랜덤 로또 생성 유틸 구현

---
+++ src/main/java/lotto/util/randomNumber/RandomLottoExtraction.java
@@ -0,0 +1,22 @@
+package lotto.util.randomNumber;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.Collections;
+import java.util.List;
+
+public final class RandomLottoExtraction {
+    private static final int LOTTO_START_NUMBER = 1;
+    private static final int LOTTO_LAST_NUMBER = 45;
+    private static final int LOTTO_NUMBER_COUNT = 6;
+
+
+    private RandomLottoExtraction() {
+    }
+
+    public static List<Integer> randomLottoExtraction() {
+        List<Integer> newLotto = Randoms.pickUniqueNumbersInRange(LOTTO_START_NUMBER, LOTTO_LAST_NUMBER,
+                LOTTO_NUMBER_COUNT);
+        Collections.sort(newLotto);
+        return newLotto;
+    }
+}
