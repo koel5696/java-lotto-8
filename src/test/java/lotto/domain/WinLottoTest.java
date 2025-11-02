d2fd53500fc9264d85ee00cc9be0d27878096e54
윤민수 <koelysa5696@naver.com>
2025-11-03 00:41:50 +0900

    test: 인자값 최적화 및 중복 테스트 삭제
    - WinLotto는 로또 번호와 보너스 번호의 중복 여부만 체크하며 나머지는 중복 검증

--- src/test/java/lotto/domain/WinLottoTest.java
+++ src/test/java/lotto/domain/WinLottoTest.java
@@ -3,43 +3,24 @@ package lotto.domain;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class WinLottoTest {
+    private static List<Integer> numbers;
 
     @Test
     void 당첨_번호가_정상적으로_생성된다() {
-        List<Integer> lotto = Arrays.stream("1,2,3,4,5,6".split(","))
-                .map(Integer::parseInt)
-                .toList();
-        int inputBonus = 10;
-
-        BonusNumber bonusNumber = new BonusNumber();
-        assertThatCode(() -> new WinLotto(new Lotto(lotto), new BonusNumber(inputBonus))
-                .doesNotThrowAnyException());
+        numbers = List.of(1, 2, 3, 4, 5, 6);
+        assertThatCode(() -> new WinLotto(new Lotto(numbers), new BonusNumber(10)))
+                .doesNotThrowAnyException();
     }
 
     @Test
-    void 보너스_번호가_이상하면_당첨_번호가_생성되지_않는다() {
-        List<Integer> lotto = Arrays.stream("1,2,3,4,5,6".split(","))
-                .map(Integer::parseInt)
-                .toList();
-        int inputBonus = -2;
-
-        assertThatThrownBy(() -> new WinLotto(new Lotto(lotto), new BonusNumber(inputBonus)))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 로또_입력이_이상하면_당첨_번호가_생성되지_않는다() {
-        List<Integer> lotto = Arrays.stream("1,2,3,4,5,6,7".split(","))
-                .map(Integer::parseInt)
-                .toList();
-        int inputBonus = 10;
-
-        assertThatThrownBy(() -> new WinLotto(new Lotto(lotto), new BonusNumber(inputBonus)))
-                .isInstanceOf(IllegalArgumentException.class);
+    void 보너스_번호가_당첨_번호와_중복되면_예외가_발생한다() {
+        numbers = List.of(1, 2, 3, 4, 5, 6);
+        assertThatThrownBy(() -> new WinLotto(new Lotto(numbers), new BonusNumber(2)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("[ERROR] 보너스 번호는 기본 당첨 번호와 중복될 수 없습니다.");
     }
 }
