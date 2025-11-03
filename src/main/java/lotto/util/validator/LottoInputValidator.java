3a9e60f03db9ea7791e2f4718c2182c855526b56
윤민수 <koelysa5696@naver.com>
2025-11-03 21:05:57 +0900

    refactor: 예외 메시지 상수화

--- src/main/java/lotto/util/validator/LottoInputValidator.java
+++ src/main/java/lotto/util/validator/LottoInputValidator.java
@@ -4,14 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class LottoInputValidator {
+    private static final String INPUT_PATTERN_ERROR_MESSAGE = "[ERROR] 쉼표(,)로 구분하고, 빈 값 없이 자연수로 입력해야 합니다.";
     private static final String LOTTO_NUMBERS_INPUT_PATTERN = "^\\d+(,\\d+)*$";
+    private static final String LOTTO_NUMBERS_SPLIT_COMMA = ",";
 
     private LottoInputValidator() {
     }
 
     public static List<Integer> lottoNumberInputValidator(String numberInput) {
         formatValidate(numberInput);
-        String[] numbers = numberInput.split(",");
+        String[] numbers = numberInput.split(LOTTO_NUMBERS_SPLIT_COMMA);
         List<Integer> numberList = new ArrayList<>();
         for (String number : numbers) {
             NumberInputValidator.numberInputValidator(number);
@@ -22,7 +24,7 @@ public final class LottoInputValidator {
 
     private static void formatValidate(String numberInput) {
         if (!numberInput.matches(LOTTO_NUMBERS_INPUT_PATTERN)) {
-            throw new IllegalArgumentException("[ERROR] 쉼표(,)로 구분하고, 빈 값 없이 자연수로 입력해야 합니다.");
+            throw new IllegalArgumentException(INPUT_PATTERN_ERROR_MESSAGE);
         }
     }
 }
--- src/main/java/lotto/util/validator/NumberInputValidator.java
+++ src/main/java/lotto/util/validator/NumberInputValidator.java
@@ -3,7 +3,11 @@ package lotto.util.validator;
 import java.math.BigInteger;
 
 public class NumberInputValidator {
-    private static final String NUMBER_INPUT_PATTERN = "^\\d*$";
+    private static final String NUMBER_INPUT_PATTERN = "^[1-9]\\d*$";
+    private static final String INPUT_EMPTY_ERROR_MESSAGE = "[ERROR] 빈(공백) 값이 들어왔습니다.";
+    private static final String INPUT_FORMAT_ERROR_MESSAGE = "[ERROR] 0으로 시작하지 않는 자연수만 입력할 수 있습니다.";
+    private static final String INPUT_OVERFLOW_ERROR_MESSAGE = "[ERROR] 입력이 비정상적으로 큽니다.";
+
 
     private NumberInputValidator() {
     }
@@ -11,26 +15,19 @@ public class NumberInputValidator {
     public static int numberInputValidator(String numberInput) {
         emptyValidate(numberInput);
         formatValidate(numberInput);
-        zeroStartValidate(numberInput);
         overflowValidate(numberInput);
         return Integer.parseInt(numberInput);
     }
 
     private static void emptyValidate(String numberInput) {
         if (numberInput == null || numberInput.isBlank()) {
-            throw new IllegalArgumentException("[ERROR] 빈(공백) 값이 들어왔습니다.");
+            throw new IllegalArgumentException(INPUT_EMPTY_ERROR_MESSAGE);
         }
     }
 
     private static void formatValidate(String numberInput) {
         if (!numberInput.matches(NUMBER_INPUT_PATTERN)) {
-            throw new IllegalArgumentException("[ERROR] 자연수로 입력해주세요.");
-        }
-    }
-
-    private static void zeroStartValidate(String numberInput) {
-        if (numberInput.charAt(0) == '0' && numberInput.length() > 1) {
-            throw new IllegalArgumentException("[ERROR] 자연수 입력은 0으로 시작할 수 없습니다.");
+            throw new IllegalArgumentException(INPUT_FORMAT_ERROR_MESSAGE);
         }
     }
 
@@ -38,7 +35,7 @@ public class NumberInputValidator {
         BigInteger numberInputAmount = new BigInteger(numberInput);
         BigInteger MaxInt = BigInteger.valueOf(Integer.MAX_VALUE);
         if (numberInputAmount.compareTo(MaxInt) > 0) {
-            throw new IllegalArgumentException("[ERROR] 입력이 비정상적으로 큽니다.");
+            throw new IllegalArgumentException(INPUT_OVERFLOW_ERROR_MESSAGE);
         }
     }
 }
