c2926f2dec7bb710093aee9e39cefc80dc318b16
윤민수 <koelysa5696@naver.com>
2025-11-03 21:37:58 +0900

    test: lottos 생성 방식 변경에 따른 테스트 수정

--- src/test/java/lotto/domain/LottosTest.java
+++ src/test/java/lotto/domain/LottosTest.java
@@ -6,17 +6,18 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.util.List;
 import java.util.Map;
-import lotto.domain.dto.LottoDTO;
+import lotto.dto.LottoDTO;
+import lotto.service.LottoService;
 import org.junit.jupiter.api.Test;
 
 public class LottosTest {
+    private static final LottoService lottoService = new LottoService();
     private static List<Integer> numbers;
 
     @Test
     void 로또_랜덤_번호가_정상적으로_생성된다() {
-
         assertRandomUniqueNumbersInRangeTest(() -> {
-                    Lottos lottos = new Lottos(new LottoPrice(3000));
+                    Lottos lottos = lottoService.generateLottosService(new LottoPrice(3000));
 
                     LottoDTO expectedDto1 = new LottoDTO(List.of(1, 2, 3, 4, 5, 6));
                     LottoDTO expectedDto2 = new LottoDTO(List.of(7, 8, 9, 10, 11, 12));
@@ -38,7 +39,7 @@ public class LottosTest {
     @Test
     void 로또_랜덤_번호가_초과되면_예외가_발생한다() {
         assertRandomUniqueNumbersInRangeTest(() -> {
-            assertThatThrownBy(() -> new Lottos(new LottoPrice(1000)))
+            assertThatThrownBy(() -> lottoService.generateLottosService(new LottoPrice(1000)))
                     .isInstanceOf(IllegalArgumentException.class);
         }, List.of(1, 2, 3, 4, 20, 46));
     }
@@ -47,7 +48,7 @@ public class LottosTest {
     void 로또_번호_비교가_정상적으로_작동한다() {
         assertRandomUniqueNumbersInRangeTest(() -> {
             numbers = List.of(1, 2, 3, 4, 20, 30);
-            Lottos lottos = new Lottos(new LottoPrice(1000));
+            Lottos lottos = lottoService.generateLottosService(new LottoPrice(1000));
             WinLotto winLotto = new WinLotto(new Lotto(numbers), new BonusNumber(10));
             Map<Rank, Integer> lottoResult = lottos.lottosCompare(winLotto);
             assertThat(lottoResult.get(Rank.FOUR)).isEqualTo(1); // 4등이 1개
@@ -58,7 +59,7 @@ public class LottosTest {
     void 보너스_당첨일_때_로또_번호_비교가_정상적으로_작동한다() {
         assertRandomUniqueNumbersInRangeTest(() -> {
             numbers = List.of(1, 2, 3, 4, 5, 30);
-            Lottos lottos = new Lottos(new LottoPrice(1000));
+            Lottos lottos = lottoService.generateLottosService(new LottoPrice(1000));
             WinLotto winLotto = new WinLotto(new Lotto(numbers), new BonusNumber(10));
             Map<Rank, Integer> lottoResult = lottos.lottosCompare(winLotto);
             assertThat(lottoResult.get(Rank.FIVE_BONUS)).isEqualTo(1); // 2등이 1개
--- src/test/java/lotto/service/LottoServiceTest.java
+++ src/test/java/lotto/service/LottoServiceTest.java
@@ -7,24 +7,25 @@ import java.util.List;
 import lotto.domain.BonusNumber;
 import lotto.domain.Lotto;
 import lotto.domain.LottoPrice;
+import lotto.domain.LottoResult;
 import lotto.domain.Lottos;
 import lotto.domain.WinLotto;
-import lotto.domain.dto.LottoResultDTO;
+import lotto.dto.LottoResultDTO;
 import org.junit.jupiter.api.Test;
 
 public class LottoServiceTest {
+    private static final LottoService lottoService = new LottoService();
     private static final List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6);
 
     @Test
     void 수익률_계산이_정상적으로_작동한다() {
         assertRandomUniqueNumbersInRangeTest(() -> {
             LottoPrice lottoPrice = new LottoPrice(1000);
-            Lottos lottos = new Lottos(lottoPrice);
+            Lottos lottos = lottoService.generateLottosService(lottoPrice);
             WinLotto winLotto = new WinLotto(new Lotto(numbers), new BonusNumber(10));
-            LottoService lottoService = new LottoService();
-            LottoResultDTO lottoResultDTO = lottoService.DrawALottoService(lottos, winLotto);
-
-            assertThat(lottoService.profitCalculatorService(lottoResultDTO, lottoPrice)).isEqualTo(150000.0);
+            LottoResult lottoResult = lottoService.DrawALottoService(lottos, winLotto);
+            LottoResultDTO lottoResultDTO = lottoService.profitCalculatorService(lottoResult, lottoPrice);
+            assertThat(lottoResultDTO.profit()).isEqualTo(150000.0);
         }, List.of(1, 2, 3, 4, 5, 30));
     }
 }
