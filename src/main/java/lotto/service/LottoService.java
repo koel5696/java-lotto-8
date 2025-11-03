d52fbc50fa41bf735f03dd732bba0a9c76641f50
윤민수 <koelysa5696@naver.com>
2025-11-03 21:05:34 +0900

    refactor: 서비스 최적화
    - lottoprice를 주입하지 않고 서비스에서 관리하여 책임 분리.
    - 직접 반환에서 dto 반환으로 변경.
    - 수익률 계산 직접하지 않고 요청하도록 변경

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,22 +1,32 @@
 package lotto.service;
 
+import java.util.ArrayList;
 import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
+import lotto.domain.Lotto;
 import lotto.domain.LottoPrice;
+import lotto.domain.LottoResult;
 import lotto.domain.Lottos;
 import lotto.domain.Rank;
 import lotto.domain.WinLotto;
-import lotto.domain.dto.LottoDTO;
-import lotto.domain.dto.LottoResultDTO;
+import lotto.dto.LottoDTO;
+import lotto.dto.LottoResultDTO;
+import lotto.util.randomNumber.RandomLottoExtraction;
 
 public class LottoService {
 
-    public Lottos generateLottoService(LottoPrice lottoPrice) {
-        return new Lottos(lottoPrice);
+    public Lottos generateLottosService(LottoPrice lottoPrice) {
+        int count = lottoPrice.lottoCount();
+
+        List<Lotto> lottos = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+            lottos.add(new Lotto(RandomLottoExtraction.randomLottoExtraction()));
+        }
+        return new Lottos(lottos);
     }
 
-    public LottoResultDTO DrawALottoService(Lottos lottos, WinLotto winLotto) {
+    public LottoResult DrawALottoService(Lottos lottos, WinLotto winLotto) {
         Map<Rank, Integer> lottoResult = new EnumMap<>(Rank.class);
         for (Rank rank : Rank.values()) {
             lottoResult.put(rank, 0);
@@ -24,25 +34,16 @@ public class LottoService {
         Map<Rank, Integer> winningResults = lottos.lottosCompare(winLotto);
         lottoResult.putAll(winningResults);
 
-        return lottos.toLottoResultDTO(lottoResult);
+        return new LottoResult(lottoResult);
     }
 
     public List<LottoDTO> generateResult(Lottos lottos) {
         return lottos.getLottosDTO();
     }
 
-    public double profitCalculatorService(LottoResultDTO lottoResultDTO, LottoPrice lottoPrice) {
-        int price = lottoPrice.lottoCount() * 1000;
-        double total = 0.0;
-        Map<Rank, Integer> lottoResult = lottoResultDTO.lottoResult();
-        for (Rank rank : lottoResult.keySet()) {
-            int prize = rank.getPrize();
-            int count = lottoResult.get(rank);
-            total += prize * count;
-        }
-        double profit = (total / price) * 100;
-        return Math.round(profit * 10) / 10.0;
+    public LottoResultDTO profitCalculatorService(LottoResult lottoResult, LottoPrice lottoPrice) {
+        double total = lottoResult.calculateTotalPrize();
+        return lottoResult.toResultDto(lottoPrice.profit(total));
     }
-
 
 }
